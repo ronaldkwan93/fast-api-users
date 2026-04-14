4b919b82fedfa9e5948a6416beaf5b7286f0a600
Ron Kwan <132134496+ronaldkwan93@users.noreply.github.com>
2026-04-14 22:35:00 +1000

    adds get user route/logic

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Path
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+users = {
+    1 : {
+        "name": "Josh",
+        "website": "www.zerotokknowing.com",
+        "age": 28,
+        "role": "developer"
+    }
+}
 
 @app.get("/")
 def root():
     return {"message": "Welcome to your Introduction to FastAPI"}
 
+#Get Users 
+@app.get("/users/{user_id}")
+def get_user(user_id:int = Path(..., description="The ID you want to get", gt=0, lt=100)):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User Not Found!")
+    return users[user_id]
